7b8bfba602aa0efbe90e9f20398eaa38a0e71efa
AshtonNich <ashtonnicholls3353@gmail.com>
2024-03-20 01:40:33 +0000

    User interface formatting

--- Algorithms-A1/MainProgram.cs
+++ Algorithms-A1/MainProgram.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,24 +13,21 @@ namespace CMP1903_A1_2324
     {
         static void Main(string[] args)
         {
-            //string Direct = @"Files\Net_1_256.txt";
-            //foreach (string line in TxtToArray.ReadTxt(Direct))
-            //{
-            //Console.WriteLine(line);
-            //}
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
             Console.WriteLine("Sorting Algorithms");
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("All file names - ");
-            Console.WriteLine("2048");
-            Console.WriteLine("256");
-            Console.WriteLine("2048-2");
-            Console.WriteLine("256-2");
-            Console.WriteLine("2048-3");
-            Console.WriteLine("256-3");
+            Console.WriteLine("2048 \n" + "256 \n" + "2048-2 \n" + "256-2 \n" + "2048-3 \n" + "256-3");
+            Console.WriteLine(("").PadRight(24, '-'));
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
             Console.WriteLine("What file would you like to sort? - ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             string userFile = Console.ReadLine();
+            
             if (userFile == "2048")
             {
                 string Direct = @"Files\Net_1_2048.txt";
+                TxtToArray.ReadTxt(Direct);
                 foreach (string line in TxtToArray.ReadTxt(Direct))
                 {
                     Console.WriteLine(line);
