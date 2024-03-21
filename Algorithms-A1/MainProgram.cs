c302bda1a2f14c4a134a6d8269cc62477799f2e3
AshtonNich <ashtonnicholls3353@gmail.com>
2024-03-21 01:52:08 +0000

    Added selection sort (needs to be added to program.cs)

--- Algorithms-A1/MainProgram.cs
+++ Algorithms-A1/MainProgram.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,60 +25,31 @@ namespace CMP1903_A1_2324
             if (userFile == "2048")
             {
                 string Direct = @"Files\Net_1_2048.txt";
-                string[] array = (TxtToArray.ReadTxt(Direct));
-                int[] lines = Array.ConvertAll(array, int.Parse);
-                LinearSort.LinSort(lines);
-                Console.WriteLine("linear sort done!\n" + "Sorted list here\n" + ("").PadRight(24, '-'));
-                foreach (int line in lines)
+                Console.WriteLine("You are reading file " + Direct + "\nWhat sort would you like to use?\n" + "Linear Sort\n" + "Selection Sort\n" + ("").PadRight(24, '-'));
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                string userSort = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                if (userSort == "Linear")
                 {
-                    Console.WriteLine(line);
+                    string[] array = (TxtToArray.ReadTxt(Direct));
+                    int[] lines = Array.ConvertAll(array, int.Parse);
+                    LinearSort.LinSort(lines);
+                    Console.WriteLine("Sorted list here\n" + ("").PadRight(24, '-'));
+                    foreach (int line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    Console.WriteLine("Linear sort done!\n" + ("").PadRight(24, '-'));
+
                 }
-                Console.WriteLine(("").PadRight(24, '-'));
+                else if (userSort == "Selection")
+                {
+
+                }
             }
             else if (userFile == "256")
             {
-                string Direct = @"Files\Net_1_256.txt";
-                foreach (string line in TxtToArray.ReadTxt(Direct))
-                {
-                    Console.WriteLine(line);
-                }
-            }
-            else if (userFile == "2048-2")
-            {
-                string Direct = @"Files\Net_2_2048.txt";
-                foreach (string line in TxtToArray.ReadTxt(Direct))
-                {
-                    Console.WriteLine(line);
-                }
-            }
-            else if (userFile == "256-2")
-            {
-                string Direct = @"Files\Net_2_256.txt";
-                foreach (string line in TxtToArray.ReadTxt(Direct))
-                {
-                    Console.WriteLine(line);
-                }
-            }
-            else if (userFile == "2048-3")
-            {
-                string Direct = @"Files\Net_3_2048.txt";
-                foreach (string line in TxtToArray.ReadTxt(Direct))
-                {
-                    Console.WriteLine(line);
-                }
-            }
-            else if (userFile == "256-3")
-            {
-                string Direct = @"Files\Net_3_256.txt";
-                foreach (string line in TxtToArray.ReadTxt(Direct))
-                {
-                    Console.WriteLine(line);
-                }
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Invalid file name");
+
             }
 
         }
---
+++ Algorithms-A1/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_A1
+{
+    internal class SelectionSort
+    {
+        public static void SelSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
